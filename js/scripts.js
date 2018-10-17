08ac150a520e33e5d1a7ba08580c9ed7114f7b51
Glen Sale and Leilani Leach <leilani@>
2018-10-17 13:23:18 -0700

    Add new features subtract, divide, multiplt

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ var subtract = function(number1, number2) {
 return number1 - number2;
 };
 
-var devide = function(number1, number2) {
+var divide = function(number1, number2) {
 return number1 / number2;
 };
 
@@ -21,5 +21,26 @@ $(document).ready(function(){
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
      $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+     $("#outputSubtract").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+     $("#outputMultiply").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+     $("#outputDivide").text(result);
   });
 });
